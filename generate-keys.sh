0b63a7106d06c88e84b4362a2321380101458dab
Adam Neumann <noizwaves@users.noreply.github.com>
2018-02-14 14:19:41 -0700

    Spin up minio as an alternative to using a real S3 bucket - set s3 resource endpoint to http://concourse-storage:9000 in pipeline or variables file to use minio

--- generate-keys.sh
+++ generate-keys.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# from https://gist.github.com/earthgecko/3089509
+random-string()
+{
+    cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
+}
+
 mkdir -p keys/web keys/worker
 
 ssh-keygen -t rsa -f ./keys/web/tsa_host_key -N ''
@@ -9,3 +15,8 @@ ssh-keygen -t rsa -f ./keys/worker/worker_key -N ''
 
 cp ./keys/worker/worker_key.pub ./keys/web/authorized_worker_keys
 cp ./keys/web/tsa_host_key.pub ./keys/worker
+
+mkdir -p keys/storage
+
+echo "MINIO_ACCESS_KEY=$(random-string 20)" > ./keys/storage/storage.env
+echo "MINIO_SECRET_KEY=$(random-string 40)" >> ./keys/storage/storage.env
